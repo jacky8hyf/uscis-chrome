ca00c45040765affc7a64196575a9b812b859a30
Elsk Hong <jacky8hyf@gmail.com>
2016-05-04 19:28:14 -0700

    good hardcoded width

--- js/background.js
+++ js/background.js
@@ -1,7 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('opening...')
   chrome.tabs.create({'url': chrome.extension.getURL('index.html')}, function(tab) {
     // Tab opened.
-    console.log('opened')
   });
 });
--- js/main.js
+++ js/main.js
@@ -1,37 +1,58 @@
 var mainFunctions = (function() {
     // local vars
-    var BASEURL = "https://egov.uscis.gov/casestatus/mycasestatus.do"
+    var BASEURL = "https://egov.uscis.gov/casestatus/mycasestatus.do";
 
-    function pad(n, width, z) {
-        z = z || '0';
-        n = n + '';
-        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    function fetchStatus(receiptNum) {
+        var dfd = jQuery.Deferred();
+        $.post(BASEURL, {appReceiptNum : receiptNum}).done(function(html) {
+            html = html.replace(/<img[^>]*>/g,""); // http://stackoverflow.com/a/15113974
+            var page = $(html);
+            var error = page.find('#formErrorMessages').text();
+            if(!/^\s*$/.test(error)) {
+                var errmsg = page.find('#formErrorMessages li').text();
+                dfd.reject(errmsg);
+                return;
+            }
+            var msg = page.find('form[name=caseStatusForm] h1').text();
+            if(!/^\s*$/.test(msg)) {
+                dfd.resolve(msg);
+                return;
+            }
+            dfd.reject('could not analyze reponse from USCIS.');
+        }).fail(function(e) {
+            dfd.reject(e);
+        });
+        return dfd.promise();
     }
 
     function onload() {
         // onload here
-        for(var i = 0; i < 500; i++)
-            fetch('WAC1690351' + pad(i, 3))
-    }
-
-    function fetch(receiptNum) {
-        $.post(BASEURL, {appReceiptNum : receiptNum}).done(function(html) {
-            html = html.replace(/<img[^>]*>/g,""); // http://stackoverflow.com/a/15113974
-            var page = $(html)
-            var error = page.find('#formErrorMessages').text()
-            if(!/^\s*$/.test(error)) {
-                var errmsg = page.find('#formErrorMessages li').text()
-                console.log(receiptNum, 'error ', errmsg)
-                return
+        $('.btn-query').click(function(event) {
+            event.preventDefault();
+            var receiptNum = $('.form-query .input-receipt-num').val()
+            var count = parseInt($('.form-query .input-count').val());
+            var prefix = receiptNum.substr(0, 3);
+            var start = parseInt(receiptNum.substr(3));
+            var table = $('.table-query-result');
+            table.find('tbody').html(
+                tmpl(table.attr('template-id'), {count: count}));
+            for(var i = 0; i < count; i++) {
+                (function(num, i){
+                    fetchStatus(prefix + num).then(function(msg) {
+                        var row = table.find('.table-row-' + i);
+                        row.find('.table-col-receipt-num').text(prefix + num);
+                        row.find('.table-col-result').text(msg)
+                    }, function(msg){
+                        var row = table.find('.table-row-' + i);
+                        row.find('.table-col-receipt-num').text(prefix + num);
+                        row.find('.table-col-result').text(msg)
+                    })
+                })(start + i, i);
             }
-            var msg = page.find('form[name=caseStatusForm] h1').text()
-            if(!/^\s*$/.test(msg)) {
-                console.log(receiptNum, 'success ', msg)
-                return
-            }
-            console.log(receiptNum, 'ERROR: COULD NOT EXTRACT INFO.')
         })
     }
+
+
 
 
     return {
